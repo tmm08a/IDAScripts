eaf02306f574a48c8723ef340cdad6a51af1fe9f
Tim Michaud <tmm08a@acu.edu>
2019-03-17 20:25:42 -0500

    jtoolCompanionFileImporter
    Useful for combining output from joker with the extracted kext.

---
+++ jtoolCompanionFileImporter.py
@@ -0,0 +1,21 @@
+import idaapi, idc, idautils
+
+#make sure IDA.cfg allows _ in function names (It does by default)
+#don't forget to double \'s 
+FILE="<CompanionFileHere>"
+
+def do_rename(line):
+	presplitline = line.split(":")
+	address = presplitline[0]
+	newName = presplitline[1].replace("\r", "").replace("\n", "")
+	address = int(address,16) #16 = base
+	#print "%s %s" % (address,newName)
+	idc.MakeName(address, newName)
+	
+
+if __name__ == "__main__":
+	f = open(FILE, "r")
+	for line in f:
+		do_rename(line)
+	f.close()
+	print "Done!\n"
